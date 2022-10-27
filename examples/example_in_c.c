2cc5e882feb9e9ff71059656d020aeb84f568ab2
Milan Skocic <milan.skocic@gmail.com>
2022-10-27 12:17:00 +0200

    ENH: Add examples.

---
+++ examples/example_in_c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "codata.h"
+
+
+int main(int argc, char **argv){
+
+    printf("alpha particle mass= %+25.16e\n", ALPHA_PARTICLE_MASS);
+
+    return 0;
+}
---
+++ examples/example_in_fortran.f90
@@ -0,0 +1,7 @@
+program example_in_fortran
+    use codata
+    implicit none
+
+    print '(A, SP,ES25.16)', "Alpha particle mass= ", ALPHA_PARTICLE_MASS
+
+end program
