53b6d9e560ebca4774aa08808787bbbc4da303a6
Milan Skocic <milan.skocic@icloud.com>
2022-12-09 19:47:20 +0100

    TST: Add first tests.

---
+++ tests/main.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "codata.h"
+ 
+ 
+ /**
+ * @brief Round off a value with n digits
+ * @param x Value to be rounded
+ * @param n Number of digits
+ * @return Rounded x.
+ */
+double roundn(double x, int n)
+{
+
+  double rounded_x;
+  double fac = pow(10, n);
+
+  rounded_x = round(x * fac) / fac;
+
+  return rounded_x;
+}
+
+int main(int argc, char **argv){
+
+    int precision;
+    double value, ref;
+    
+    value = ALPHA_PARTICLE_MASS;
+    ref = 6.6446573357e-27;
+    precision = 10;
+    printf("value=%+23.16e / ref=%+23.16e / precision=%d\n", value, ref, precision);
+    if (roundn(value, precision) != roundn(ref, precision)){
+        return 1;
+    }
+
+    value = U_ALPHA_PARTICLE_MASS;
+    ref = 0.0000000020e-27;
+    precision = 10;
+    printf("value=%+23.16e / ref=%+23.16e / precision=%d\n", value, ref, precision);
+    if (roundn(value, precision) != roundn(ref, precision)){
+        return 1;
+    }
+
+
+    return 0;
+}
