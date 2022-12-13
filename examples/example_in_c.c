3b1aa9a502d7dd9159adce7988769d6d402ef408
Milan Skocic <milan.skocic@icloud.com>
2022-12-13 11:32:31 +0100

    DEV: Update examples.

--- examples/example_in_c.c
+++ examples/example_in_c.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "codata.h"
 
-
 int main(int argc, char **argv){
 
-    printf("alpha particle mass= %+25.16e\n", ALPHA_PARTICLE_MASS);
+    codata_print();
     return 0;
 }
--- examples/example_in_fortran.f90
+++ examples/example_in_fortran.f90
@@ -2,6 +2,6 @@ program example_in_fortran
     use codata
     implicit none
 
-    print *, codata_constants(1)%name, codata_constants(1)%value
+    call codata_print()
 
 end program
