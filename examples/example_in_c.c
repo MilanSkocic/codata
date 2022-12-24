95e3943be82e69d851c56b1725ba725f895be49a
Milan Skocic <milan.skocic@icloud.com>
2022-12-24 13:12:45 +0100

    DEV: Reset examples.

--- examples/example_in_c.c
+++ examples/example_in_c.c
@@ -7,8 +7,5 @@ int main(int argc, char **argv){
     codata_capi_print();
     char name[] = "alpha particle mass";
     printf("%+23.16e\n", codata_capi_get_value(name, strlen(name)));
-
-    printf("%s\n", codata_capi_get_unit_by_index(1));
-
     return 0;
 }
--- examples/example_in_fortran.f90
+++ examples/example_in_fortran.f90
@@ -2,10 +2,7 @@ program example_in_fortran
     use codata
     implicit none
 
-    ! call codata_print()
-
-    print *,codata_get_value("alpha particle mass")
-
-    print *, codata_get_unit_by_index(2)
+    call codata_print()
+    print *, codata_get_value("alpha particle mass")
 
 end program
