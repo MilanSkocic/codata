5b7cdd922e085964ae2bd7a4e217fa6a3708828c
Milan Skocic <milan.skocic@icloud.com>
2023-01-22 12:20:32 +0100

    ENH: Improve examples.

--- examples/example_in_c.c
+++ examples/example_in_c.c
@@ -4,8 +4,17 @@
 
 int main(int argc, char **argv){
 
+    char year[5] = "2014";
+
+    /* call directly codata, the values used will be the last i.e. 2018 */
     codata_capi_print();
     char name[] = "alpha particle mass";
     printf("%+23.16e\n", codata_capi_get_value(name, strlen(name)));
     return 0;
+
+    /* Or set the codata values that you want */
+    set_codata_capi(year, 4);
+    printf("%+23.16e\n", codata_capi_get_value(name, strlen(name)));
+    return 0;
+
 }
--- examples/example_in_fortran.f90
+++ examples/example_in_fortran.f90
@@ -2,7 +2,16 @@ program example_in_fortran
     use codata
     implicit none
 
+    character(len=4) :: year = "2014"
+
+    ! call directly codata, the values used will be the last i.e. 2018
     call codata_print()
     print *, codata_get_value("alpha particle mass")
+
+    ! Or set the codata values that you want
+    call set_codata(year)
+    call codata_print()
+    print *, codata_get_value("alpha particle mass")
+
 
 end program
