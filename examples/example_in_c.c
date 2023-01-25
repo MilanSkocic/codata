beaaf12d39bdc0213b09b6410ea5cd7d74dfbc78
Milan Skocic <milan.skocic@icloud.com>
2023-01-25 14:14:45 +0100

    ENH: Improve examples.

--- examples/example_in_c.c
+++ examples/example_in_c.c
@@ -9,12 +9,11 @@ int main(int argc, char **argv){
     /* call directly codata, the values used will be the last i.e. 2018 */
     codata_capi_print();
     char name[] = "alpha particle mass";
-    printf("%+23.16e\n", codata_capi_get_value(name, strlen(name)));
-    return 0;
+    printf("Codata 2018: %+23.16e\n", codata_capi_get_value(name, strlen(name)));
 
     /* Or set the codata values that you want */
     set_codata_capi(year, 4);
-    printf("%+23.16e\n", codata_capi_get_value(name, strlen(name)));
+    printf("Codata 2014: %+23.16e\n", codata_capi_get_value(name, strlen(name)));
     return 0;
 
 }
--- examples/example_in_fortran.f90
+++ examples/example_in_fortran.f90
@@ -6,12 +6,11 @@ program example_in_fortran
 
     ! call directly codata, the values used will be the last i.e. 2018
     call codata_print()
-    print *, codata_get_value("alpha particle mass")
+    print *, "Codata 2018: ", codata_get_value("alpha particle mass")
 
     ! Or set the codata values that you want
     call set_codata(year)
-    call codata_print()
-    print *, codata_get_value("alpha particle mass")
+    print *, "Codata 2014: ", codata_get_value("alpha particle mass")
 
 
 end program
