56e2d30be874494bf07b5d95ecaf4b7b774fae7a
Milan Skocic <milan.skocic@icloud.com>
2023-01-30 15:12:43 +0100

    DEV: Update examples with new getter.

--- examples/example_in_c.c
+++ examples/example_in_c.c
@@ -10,10 +10,13 @@ int main(int argc, char **argv){
     codata_capi_print();
     char name[] = "alpha particle mass";
     printf("Codata 2018: %+23.16e\n", codata_capi_get_value(name, strlen(name)));
+    printf("%s\n", codata_get_year_capi());
 
     /* Or set the codata values that you want */
     codata_set_year_capi(year, 4);
     printf("Codata 2014: %+23.16e\n", codata_capi_get_value(name, strlen(name)));
+    printf("%s\n", codata_get_year_capi());
+    
     return 0;
 
 }
--- examples/example_in_fortran.f90
+++ examples/example_in_fortran.f90
@@ -7,10 +7,13 @@ program example_in_fortran
     ! call directly codata, the values used will be the last i.e. 2018
     call codata_print()
     print *, "Codata 2018: ", codata_get_value("alpha particle mass")
+    print *, codata_get_year()
 
     ! Or set the codata values that you want
     call codata_set_year(year)
     print *, "Codata 2014: ", codata_get_value("alpha particle mass")
 
+
+    print *, codata_get_year()
 
 end program
