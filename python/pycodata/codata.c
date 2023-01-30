c93155645cd348e3a09594e25dedc787555fb686
Milan Skocic <milan.skocic@icloud.com>
2023-01-30 15:12:58 +0100

    DEV: Update python wrapper with new getter.

--- python/pycodata/codata.c
+++ python/pycodata/codata.c
@@ -1,9 +1,18 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <stdio.h>
 #include <string.h>
 #include "codata.h"
 
-PyDoc_STRVAR(module_docstring, "Codata constants.");
+PyDoc_STRVAR(module_docstring, "Codata constants from NIST.");
+
+PyDoc_STRVAR(codata_set_year_doc, 
+"set_year(year=last) --> None \n\n"
+"Set the revision year for the codata constants.");
+
+PyDoc_STRVAR(codata_get_year_doc, 
+"get_year() --> str \n\n"
+"Get the revision year for the codata constants.");
 
 PyDoc_STRVAR(codata_print_doc, 
 "print() --> None \n\n"
@@ -17,10 +26,33 @@ PyDoc_STRVAR(codata_get_unit_doc,
 "get_unit(name) --> float \n\n"
 "Get the unit of the constant defined by name. If not found returns \"None\"");
 
-
 PyDoc_STRVAR(codata_as_dict_doc, 
 "as_dict() --> dict \n\n"
 "Return all constants available in a dictionnary");
+
+static PyObject *_codata_set_year(PyObject *self, PyObject *args, PyObject *kwargs){
+    
+    static char *kwlist[] = {"year", NULL};
+    char _year[5] = "XXXX";
+    char *year = _year;
+    
+    if(!PyArg_ParseTupleAndKeywords(args, kwargs, "|s", kwlist, &year)){
+        PyErr_SetString(PyExc_TypeError, "year must be of type str.");
+        return NULL;
+    }
+
+    codata_set_year_capi(year, strlen(year));
+
+    Py_RETURN_NONE;
+
+}
+
+static PyObject *_codata_get_year(PyObject *self, PyObject *args)
+{
+    char *year; 
+    year = codata_get_year_capi();
+    return Py_BuildValue("s", year);
+}
 
 static PyObject *_codata_print(PyObject *self, PyObject *args)
 {
@@ -93,7 +125,9 @@ static PyObject *_codata_constants_as_dict(PyObject *self, PyObject *args){
 }
 
 static PyMethodDef myMethods[] = {
-    { "print", (PyCFunction) _codata_print, METH_NOARGS, codata_print_doc },
+    {"set_year", (PyCFunction) _codata_set_year, METH_VARARGS|METH_KEYWORDS, codata_set_year_doc },
+    {"get_year", (PyCFunction) _codata_get_year, METH_NOARGS, codata_get_year_doc },
+    {"print", (PyCFunction) _codata_print, METH_NOARGS, codata_print_doc },
     {"get_value", (PyCFunction) _codata_get_value, METH_VARARGS, codata_get_value_doc},
     {"get_unit", (PyCFunction) _codata_get_unit, METH_VARARGS, codata_get_unit_doc},
     {"as_dict", (PyCFunction) _codata_constants_as_dict, METH_NOARGS, codata_as_dict_doc},
--- python/run.py
+++ python/run.py
@@ -3,6 +3,9 @@ from pycodata import codata
 
 value = codata.get_value("alpha particle mass")
 print(value)
+print(codata.get_year())
+codata.set_year("2014")
 
-value = codata.get_unit("triton relative atomic mass")
+value = codata.get_value("alpha particle mass")
 print(value)
+print(codata.get_year())
