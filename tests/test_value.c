796c0f4b3619ddc34b857ba09c0d70ec4761194c
Milan Skocic <milan.skocic@icloud.com>
2023-02-06 07:36:39 +0100

    TST: Add equivalent test for codata 2010.

--- tests/test_value.c
+++ tests/test_value.c
@@ -36,6 +36,18 @@ int main(int argc, char **argv){
     if (roundn(value, precision) != roundn(ref, precision)){
         return EXIT_FAILURE;
     }
+    
+    printf("Test CODATA 2010\n");
+    strcpy(year, "2010");
+    codata_capi_set_year(year, 4);
+    strcpy(name, "alpha particle mass");
+    value = codata_capi_get_value(name, strlen(name)); 
+    ref = 6.644657230e-27;
+    precision = 37;
+    printf("value=%+23.16e / ref=%+23.16e / precision=%d\n", value, ref, precision);
+    if (roundn(value, precision) != roundn(ref, precision)){
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
