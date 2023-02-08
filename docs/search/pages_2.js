fa35fc673687f9040b2222ab05ae9acdd647bb75
Milan Skocic <milan.skocic@icloud.com>
2023-02-08 07:36:08 +0100

    DOC: Add requirements in documentation + online update.

--- docs/search/pages_2.js
+++ docs/search/pages_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['raw_5fcodata_0',['raw_codata',['../md_raw_codata.html',1,'']]]
+  ['raw_5fcodata_0',['raw_codata',['../md_raw_codata.html',1,'']]],
+  ['requirements_1',['requirements',['../md_requirements.html',1,'']]]
 ];
