f4b019295a5ed661ffc2fb87b45d2c459c5ce1ea
Milan Skocic <milan.skocic@icloud.com>
2023-03-21 09:45:54 +0100

    DOC: Update online documentation.

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -11,7 +11,7 @@ var searchData=
   ['codata_200_2e1_2e0_20release_20note_8',['Codata 0.1.0 Release Note',['../md_doxygen_releases_0_1_0_notes.html',1,'']]],
   ['codata_200_2e2_2e0_20release_20note_9',['Codata 0.2.0 Release Note',['../md_doxygen_releases_0_2_0_notes.html',1,'']]],
   ['codata_200_2e2_2e1_20release_20note_10',['Codata 0.2.1 Release Note',['../md_doxygen_releases_0_2_1_notes.html',1,'']]],
-  ['codata_200_2e2_2e2_20release_20note_11',['Codata 0.2.2 Release Note',['../md_doxygen_releases_0_2_2_notes.html',1,'']]],
+  ['codata_200_2e3_2e0_20release_20note_11',['Codata 0.3.0 Release Note',['../md_doxygen_releases_0_3_0_notes.html',1,'']]],
   ['codata_20raw_20data_12',['Codata Raw Data',['../md_doxygen_introduction_raw_codata.html',1,'']]],
   ['codata_2ef90_13',['codata.f90',['../codata_8f90.html',1,'']]],
   ['codata_2eh_14',['codata.h',['../codata_8h.html',1,'']]],
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -3,6 +3,6 @@ var searchData=
   ['codata_200_2e1_2e0_20release_20note_0',['Codata 0.1.0 Release Note',['../md_doxygen_releases_0_1_0_notes.html',1,'']]],
   ['codata_200_2e2_2e0_20release_20note_1',['Codata 0.2.0 Release Note',['../md_doxygen_releases_0_2_0_notes.html',1,'']]],
   ['codata_200_2e2_2e1_20release_20note_2',['Codata 0.2.1 Release Note',['../md_doxygen_releases_0_2_1_notes.html',1,'']]],
-  ['codata_200_2e2_2e2_20release_20note_3',['Codata 0.2.2 Release Note',['../md_doxygen_releases_0_2_2_notes.html',1,'']]],
+  ['codata_200_2e3_2e0_20release_20note_3',['Codata 0.3.0 Release Note',['../md_doxygen_releases_0_3_0_notes.html',1,'']]],
   ['codata_20raw_20data_4',['Codata Raw Data',['../md_doxygen_introduction_raw_codata.html',1,'']]]
 ];
