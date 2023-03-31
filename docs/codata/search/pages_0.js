e8f4ef5283b1b99173308e0c15a685245e395cf0
Milan Skocic <milan.skocic@icloud.com>
2023-03-31 07:08:27 +0200

    DOC: Update online documentation.

--- docs/codata/search/pages_0.js
+++ docs/codata/search/pages_0.js
@@ -4,5 +4,6 @@ var searchData=
   ['codata_200_2e2_2e0_20release_20note_1',['Codata 0.2.0 Release Note',['../md_doxygen_releases_0_2_0_notes.html',1,'']]],
   ['codata_200_2e2_2e1_20release_20note_2',['Codata 0.2.1 Release Note',['../md_doxygen_releases_0_2_1_notes.html',1,'']]],
   ['codata_200_2e3_2e0_20release_20note_3',['Codata 0.3.0 Release Note',['../md_doxygen_releases_0_3_0_notes.html',1,'']]],
-  ['codata_20raw_20data_4',['Codata Raw Data',['../md_doxygen_introduction_raw_codata.html',1,'']]]
+  ['codata_200_2e4_2e0_20release_20note_4',['Codata 0.4.0 Release Note',['../md_doxygen_releases_0_4_0_notes.html',1,'']]],
+  ['codata_20raw_20data_5',['Codata Raw Data',['../md_doxygen_introduction_raw_codata.html',1,'']]]
 ];
--- doxygen/introduction/requirements.md
+++ doxygen/introduction/requirements.md
@@ -2,4 +2,4 @@ gcc>=4.6 or msvc>=14
 
 gfortran>=4.6 or ifort>=18
 
-cmake>=3.20
+cmake>=3.10
