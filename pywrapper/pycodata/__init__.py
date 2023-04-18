239a11ed58a13e119e8c76ec2ab013d0e5e67a44
Milan Skocic <milan.skocic@icloud.com>
2023-04-18 11:10:42 +0200

    MAINT: Remove test for C Extension.

--- pywrapper/pycodata/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Python module containing the codata constants."""
-from .version import *
-from .codata import *
--- pywrapper/tests/test_lib.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import pycodata
-
-class TestLib(unittest.TestCase):
-    r"""Test pyiawps library."""
-    def test_value(self):
-        value = pycodata.ALPHA_PARTICLE_MASS
-        reference = 6.6446573357e-27 
-        self.assertEqual(value, reference)
