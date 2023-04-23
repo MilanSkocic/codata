82c1b38dc866d629f230d905ae340bebcb961b20
Milan Skocic <milan.skocic@icloud.com>
2023-04-23 09:39:34 +0200

    DOC: Generate online doc.

--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -5,5 +5,6 @@ var searchData=
   ['0_2e2_2e1_2dnotes_2emd_2',['0.2.1-notes.md',['../0_82_81-notes_8md.html',1,'']]],
   ['0_2e3_2e0_2dnotes_2emd_3',['0.3.0-notes.md',['../0_83_80-notes_8md.html',1,'']]],
   ['0_2e4_2e0_2dnotes_2emd_4',['0.4.0-notes.md',['../0_84_80-notes_8md.html',1,'']]],
-  ['0_2e5_2e0_2dnotes_2emd_5',['0.5.0-notes.md',['../0_85_80-notes_8md.html',1,'']]]
+  ['0_2e5_2e0_2dnotes_2emd_5',['0.5.0-notes.md',['../0_85_80-notes_8md.html',1,'']]],
+  ['0_2e6_2e0_2dnotes_2emd_6',['0.6.0-notes.md',['../0_86_80-notes_8md.html',1,'']]]
 ];
