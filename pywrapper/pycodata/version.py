9ddee347b486ac4e2342f04a1860c2e4e3232bb3
Milan Skocic <milan.skocic@icloud.com>
2023-04-28 01:39:46 +0200

    ENH: Update wrapper and version.

--- pywrapper/pycodata/version.py
+++ pywrapper/pycodata/version.py
@@ -11,7 +11,7 @@ __all__ = ['__package_name__',
            '__license__']
 
 __package_name__ = "pycodata"
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 __author__ = "Milan Skocic"
 __author_email__ = "milan.skocic@icloud.com"
 __maintainer__ = __author__
