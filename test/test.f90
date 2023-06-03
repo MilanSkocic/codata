bb9c00ee8d9491231f7d6bf0e3aba68c8fd79115
Milan Skocic <milan.skocic@icloud.com>
2023-06-03 07:28:49 +0200

    TST: rounding function in tests.

--- test/test.f90
+++ test/test.f90
@@ -1,9 +1,21 @@
 program main
+    use iso_fortran_env
     use codata
     implicit none
     
-    print *,  SPEED_OF_LIGHT_IN_VACUUM
-    print *, U_SPEED_OF_LIGHT_IN_VACUUM
+    print *, "c=",  SPEED_OF_LIGHT_IN_VACUUM
+    print *, "U(c)=", U_SPEED_OF_LIGHT_IN_VACUUM
 
+contains
+
+pure elemental function roundn(x, n)result(r)
+    implicit none
+    real(real64), intent(in) :: x
+    integer(int32), intent(in) :: n
+    real(real64) :: r
+
+    r = anint(x * 10.0d0**n, kind=kind(r)) / 10.0d0**n
+
+end function
 
 end program main
