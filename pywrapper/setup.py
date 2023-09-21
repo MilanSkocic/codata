4313e86f5d592d9fa06bb8eba23ca488464339d8
Milan Skocic <milan.skocic@icloud.com>
2023-09-21 07:58:22 +0200

    BLD: Migrate to pyproject.toml

--- pywrapper/setup.py
+++ pywrapper/setup.py
@@ -2,12 +2,7 @@ r"""setup"""
 import importlib
 import pathlib
 import platform
-from setuptools import setup, find_packages, Extension
-
-# Import only version.py file for extracting the version
-spec = importlib.util.spec_from_file_location('version', './pycodata/version.py')
-mod = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mod)
+from setuptools import setup, Extension
 
 libraries = None
 library_dirs = None
@@ -34,27 +29,7 @@ if __name__ == "__main__":
                         runtime_library_dirs=runtime_library_dirs,
                         extra_objects=extra_objects)
     
-    setup(name=mod.__package_name__,
-        version=mod.__version__,
-        maintainer=mod.__maintainer__,
-        maintainer_email=mod.__maintainer_email__,
-        author=mod.__author__,
-        author_email=mod.__author_email__,
-        description=mod.__package_name__,
-        long_description=pathlib.Path("README.rst").read_text(encoding="utf-8"),
-        long_description_content_type="text/x-rst",
-        url='https://milanskocic.github.io/codata/index.html',
-        download_url='https://github.com/MilanSkocic/codata',
-        packages=find_packages(),
-        include_package_data=False,
-        package_data={"pycodata": ["libcodata.*"]},
-        python_requires='>=3.8',
-        install_requires=pathlib.Path("requirements.txt").read_text(encoding="utf-8").split('\n'),
-        classifiers=["Development Status :: 5 - Production/Stable",
-                    "Intended Audience :: Science/Research",
-                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
-        ext_modules=[mod_ext]
-        )
+    setup(ext_modules=[mod_ext])
 
 
 # pypi
