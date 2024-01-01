cd211a400d1f4735a5af6835e38fb6422c6fc275
Milan Skocic <milan.skocic@icloud.com>
2024-01-01 07:33:46 +0100

    ENH: C API in the same file as the Fortran module.

--- src/codata.f90
+++ src/codata.f90
@@ -1,7 +1,6 @@
 module codata
 !! Main module for CODATA library.
 use codata__version
-use codata__version_capi
 use codata__constants
 implicit none
 
--- src/codata_version.f90
+++ src/codata_version.f90
@@ -1,12 +1,13 @@
 module codata__version
 !! Version
 use iso_fortran_env
+use iso_c_binding
 implicit none
 private
 
 character(len=*), parameter :: version = "0.9.0"
 character(len=:), allocatable, target :: version_f
-
+character(len=:), allocatable, target :: version_c
 
 public :: get_version
 
@@ -26,6 +27,26 @@ function get_version()result(fptr)
     allocate(character(len=len(version)) :: version_f)
     version_f = version
     fptr => version_f    
+end function
+
+function capi_get_version()bind(c,name="codata_get_version")result(cptr)
+    !! Get the version.
+    implicit none
+    
+    ! Returns   
+    type(c_ptr) :: cptr
+        !! Pointer to version string.
+
+    character(len=:), pointer :: fptr
+    fptr => get_version() 
+
+    if(allocated(version_c))then
+        deallocate(version_c)
+    endif
+    allocate(character(len=len(fptr)+1) :: version_c)
+
+    version_c = fptr // c_null_char
+    cptr = c_loc(fptr)
 end function
 
 end module codata__version
