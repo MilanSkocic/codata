94072f0a70a3d201ca6725b7d0be9c18ebe631bd
Milan Skocic <milan.skocic@icloud.com>
2024-01-16 06:37:30 +0100

    ENH: Refractor and clean examples.

--- example/example_in_f.f90
+++ example/example_in_f.f90
@@ -3,8 +3,13 @@ program example_in_f
     use codata
     implicit none
     
+    print '(A)', '########## VERSION ##########'
     print *, "version = ", get_version()
     
+    print '(A)', '########## CONSTANTS ##########'
     print *, "c=",  speed_of_light_in_vacuum
+    
+    print '(A)', '########## UNCERTAINTY ##########'
     print *, "u(c)=", u_speed_of_light_in_vacuum
+
 end program
--- example/example_in_py.py
+++ example/example_in_py.py
@@ -1,4 +1,13 @@
+r"""Example in python."""
+import sys
+sys.path.append("../pywrapper")
 import pycodata
 
+print("########## VERSION ##########")
 print(pycodata.__version__)
+
+print("########## CONSTANTS ##########")
 print(pycodata.SPEED_OF_LIGHT_IN_VACUUM)
+
+print("########## UNCERTAINTY ##########")
+print(pycodata.U_SPEED_OF_LIGHT_IN_VACUUM)
