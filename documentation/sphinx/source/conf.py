a8fb562c18d588f40be0051bc38b450e9b4e76dd
Milan Skocic <milan.skocic@icloud.com>
2024-01-20 00:40:12 +0100

    RLS: Prepare new release.

--- documentation/sphinx/source/conf.py
+++ documentation/sphinx/source/conf.py
@@ -42,7 +42,7 @@ author = "M. Skocic"
 # built documents.
 #
 # The short X.Y version.
-version = "0.9.0" 
+version = "0.10.0" 
 # The full version, including alpha/beta/rc tags.
 release = version
 
