05b24743ef0a57ea9284af65e54bdd39f6e05653
Milan Skocic <milan.skocic@icloud.com>
2024-03-07 07:28:30 +0100

    ENH: Switch to doxygen markups.

--- src/codata_constants.f90
+++ src/codata_constants.f90
@@ -1,6 +1,13 @@
+!> @file
+!! @brief Codata constants. 
+
+!> @brief Constants module.
+!! @detail The latest values do not any year in their name.
+!! Older values than the latest can be used and they feature 
+!! a suffix in their name.
 module codata__constants
-!! Current Codata constans.
 use codata__constants_latest
 use codata__constants_2018
 use codata__constants_2014
+use codata__constants_2010
 end module codata__constants
