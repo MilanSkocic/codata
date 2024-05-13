f773cb0e10addecf39d50f0b505abdad77d2b5c2
Milan Skocic <milan.skocic@icloud.com>
2024-05-13 10:16:04 +0200

    TST: Update tests with 2022 release.

--- test/testsuite_constants.f90
+++ test/testsuite_constants.f90
@@ -40,142 +40,168 @@ subroutine test_year(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
     
-    integer(int32) :: value, expected
+    integer(int32) :: value, expected, diff
     
-    expected = 2018
+    expected = 2022
     value = YEAR
-
-    call check(error, value, expected)
+    diff = expected - value
+    call check(error, diff, 0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_ALPHA_PARTICLE_ELECTRON_MASS_RATIO(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 7294.29954142d0
-    value = ALPHA_PARTICLE_ELECTRON_MASS_RATIO
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d0
+    expected = 7294.29954171d0 * fac
+    value = ALPHA_PARTICLE_ELECTRON_MASS_RATIO * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_ALPHA_PARTICLE_MASS(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 6.6446573357d-27
-    value = ALPHA_PARTICLE_MASS
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d27
+    expected = 6.6446573450d-27 * fac
+    value = ALPHA_PARTICLE_MASS * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_ATOMIC_MASS_CONSTANT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 1.66053906660d-27
-    value = ATOMIC_MASS_CONSTANT
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d27
+    expected = 1.66053906892d-27 * fac
+    value = ATOMIC_MASS_CONSTANT * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_AVOGADRO_CONSTANT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected =  6.02214076d23
-    value = AVOGADRO_CONSTANT
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d-23
+    expected =  6.02214076d23 * fac
+    value = AVOGADRO_CONSTANT * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_BOLTZMANN_CONSTANT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected =  1.380649d-23
-    value = BOLTZMANN_CONSTANT
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d23
+    expected =  1.380649d-23 * fac
+    value = BOLTZMANN_CONSTANT * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_ELECTRON_VOLT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 1.602176634d-19
-    value = ELECTRON_VOLT 
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d19
+    expected = 1.602176634d-19 * fac
+    value = ELECTRON_VOLT * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_ELEMENTARY_CHARGE(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 1.602176634d-19
-    value = ELEMENTARY_CHARGE
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d19
+    expected = 1.602176634d-19 * fac
+    value = ELEMENTARY_CHARGE * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_FARADAY_CONSTANT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 96485.33212d0
-    value = FARADAY_CONSTANT 
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d0
+    expected = 96485.33212d0 * fac
+    value = FARADAY_CONSTANT * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_MOLAR_MASS_CONSTANT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 0.99999999965d-3
-    value = MOLAR_MASS_CONSTANT
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d3
+    expected = 1.00000000105d-3 * fac
+    value = MOLAR_MASS_CONSTANT * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_MOLAR_VOLUME_NTP(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 22.41396954d-3
-    value = MOLAR_VOLUME_OF_IDEAL_GAS__273_15_K__101_325_KPA
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d3
+    expected = 22.41396954d-3 * fac
+    value = MOLAR_VOLUME_OF_IDEAL_GAS__273_15_K__101_325_KPA * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_PLANCK_CONSTANT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected, fac
-    expected = 6.62607015d-34 
-    value = PLANCK_CONSTANT 
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d34
+    expected = 6.62607015d-34 * fac
+    value = PLANCK_CONSTANT * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_SPEED_OF_LIGHT(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected
-    expected = 299792458.0d0
-    value = SPEED_OF_LIGHT_IN_VACUUM
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d0
+    expected = 299792458.0d0 * fac
+    value = SPEED_OF_LIGHT_IN_VACUUM * fac
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
 subroutine test_STANDARD_ACCELERATION_OF_GRAVITY(error)
     implicit none
     type(error_type), allocatable, intent(out) :: error 
-    real(real64) :: value, expected
-    expected = 9.80665d0
-    value = STANDARD_ACCELERATION_OF_GRAVITY 
-    call check(error, value, expected)
+    real(real64) :: value, expected, diff, fac
+    fac = 1.0d0
+    expected = 9.80665d0 * fac
+    value = STANDARD_ACCELERATION_OF_GRAVITY * fac 
+    diff = expected - value
+    call check(error, diff, 0.0d0)
     if (allocated(error)) return
 end subroutine
 
