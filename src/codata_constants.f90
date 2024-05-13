78c31250934da0876f4b0d8a7c48e0eb4c1e80f7
Milan Skocic <milan.skocic@icloud.com>
2024-05-13 15:24:34 +0200

    ENH: Add 2022 values in main module.

--- src/codata_constants.f90
+++ src/codata_constants.f90
@@ -6,6 +6,7 @@
 !! Older values can be used and they feature the year as a suffix in their name.
 module codata__constants
 use codata__constants_latest
+use codata__constants_2022
 use codata__constants_2018
 use codata__constants_2014
 use codata__constants_2010
