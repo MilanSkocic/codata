a655ff71013a3040cecbb562c55b8e5447a2b17e
Milan Skocic <milan.skocic@icloud.com>
2024-05-21 23:44:14 +0200

    DOC: Online doc.

---
+++ docs/src/codata_constants.f90
@@ -0,0 +1,13 @@
+!> @file
+!! @brief Codata constants. 
+
+!> @brief Constants module.
+!! @details The latest values (2022) do not have the year as a suffix in their name.
+!! Older values can be used and they feature the year as a suffix in their name.
+module codata__constants
+use codata__constants_2022
+use codata__constants_2018
+use codata__constants_2014
+use codata__constants_2010
+use codata__constants_type
+end module codata__constants
