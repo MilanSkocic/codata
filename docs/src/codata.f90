ab6a2cffe356e90c6a10cd3c44ba8d15ad660533
Milan Skocic <milan.skocic@icloud.com>
2024-05-22 06:08:45 +0200

    DOC: Generate online doc.

---
+++ docs/src/codata.f90
@@ -0,0 +1,10 @@
+!> @file
+!! @brief Main module for the Codata library.
+
+!> @brief Main module for the Codata library.
+module codata
+use codata__version
+use codata__constants
+implicit none
+
+end module
