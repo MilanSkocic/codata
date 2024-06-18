d9d4746edd0c0731ae4ebd6c74cadc98c253dfc1
Milan Skocic <milan.skocic@icloud.com>
2024-06-18 09:28:11 +0200

    ENH: Start merging all C prototypes in one header.

--- include/codata.h
+++ include/codata.h
@@ -2,5 +2,8 @@
 #define CODATA_H
 #include "codata_version.h"
 #include "codata_constants.h"
-#endif
 
+/* Version module */
+extern char* codata_get_version(void); 
+
+#endif
--- include/codata_version.h
+++
@@ -1,5 +0,0 @@
-#ifndef CODATA_VERSION_H
-#define CODATA_VERSION_H
-extern char* codata_get_version(void); 
-#endif
-
