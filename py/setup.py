fc68e1432f4903b8570041bc63ef0699f4b6c716
Milan Skocic <milan.skocic@icloud.com>
2024-11-18 05:13:10 +0100

    Switch to pure Python code in setup.py

--- py/setup.py
+++ py/setup.py
@@ -44,66 +44,70 @@ if platform.system() == "Darwin":
 
 
 # Headers
-root_src = pathlib.Path(f"./src/py{name:s}/include/")
-root_dest = pathlib.Path(f"./src/py{name:s}/")
-files = [f"{name:s}.h"]
-for file in files:
-    src = root_src / file
-    dest = root_dest / file
-    if not src.exists():
-        raise ValueError(f"The library {name:s} was not installed. Run in the root folder: make install prefix=./py/src/py{name:s}")
-    try:
-        print(f"copying {str(src):s} -> {str(dest.parent):s}.")
-        shutil.copy(src, dest)
-    except:
-        print(f"{file:s} was not copied.")
+# root_src = pathlib.Path(f"./src/py{name:s}/include/")
+# root_dest = pathlib.Path(f"./src/py{name:s}/")
+# files = [f"{name:s}.h"]
+# for file in files:
+#     src = root_src / file
+#     dest = root_dest / file
+#     if not src.exists():
+#         raise ValueError(f"The library {name:s} was not installed. Run in the root folder: make install prefix=./py/src/py{name:s}")
+#     try:
+#         print(f"copying {str(src):s} -> {str(dest.parent):s}.")
+#         shutil.copy(src, dest)
+#     except:
+#         print(f"{file:s} was not copied.")
 
-# Libs
-root_src = pathlib.Path(f"./src/py{name:s}/lib/")
-root_dest = pathlib.Path(f"./src/py{name:s}/")
-files = list(map(lambda s: f"lib{name:s}"+s, ext))
-for file in files:
-    src = root_src / file
-    dest = root_dest / file
-    if not src.exists():
-        raise ValueError(f"The library {name:s} was not installed. Run in the root folder: make install prefix=./py/src/py{name:s}")
-    try:
-        print(f"copying {str(src):s} -> {str(dest.parent):s}.")
-        shutil.copy(src, dest)
-    except:
-        print(f"{file:s} was not copied.")
+# # Libs
+# root_src = pathlib.Path(f"./src/py{name:s}/lib/")
+# root_dest = pathlib.Path(f"./src/py{name:s}/")
+# files = list(map(lambda s: f"lib{name:s}"+s, ext))
+# for file in files:
+#     src = root_src / file
+#     dest = root_dest / file
+#     if not src.exists():
+#         raise ValueError(f"The library {name:s} was not installed. Run in the root folder: make install prefix=./py/src/py{name:s}")
+#     try:
+#         print(f"copying {str(src):s} -> {str(dest.parent):s}.")
+#         shutil.copy(src, dest)
+#     except:
+#         print(f"{file:s} was not copied.")
 
-# gfortran libs
-root_src = pathlib.Path(ROOT)
-root_dest = pathlib.Path(f"./src/py{name:s}/")
-files = LIBS
-for file in files:
-    src = root_src / (file + ext[0])
-    dest = root_dest / (file + ext[0])
-    cmd = ["install_name_tool", "-change", f"{str(src):s}", f"@loader_path/{src.name}", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
-    try:
-        print(f"copying {str(src):s} -> {str(dest.parent):s}.")
-        shutil.copy(src, dest)
-        if platform.system() == "Darwin":
-            print(f"changing rpath for {str(file):s}")
-            subprocess.check_call(cmd)
-    except subprocess.CalledProcessError:
-        print(" ".join(cmd) + " was not successful.")
-    except:
-        print(f"{file:s} was not copied.")
+# # gfortran libs
+# root_src = pathlib.Path(ROOT)
+# root_dest = pathlib.Path(f"./src/py{name:s}/")
+# files = LIBS
+# for file in files:
+#     src = root_src / (file + ext[0])
+#     dest = root_dest / (file + ext[0])
+#     cmd = ["install_name_tool", "-change", f"{str(src):s}", f"@loader_path/{src.name}", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
+#     try:
+#         print(f"copying {str(src):s} -> {str(dest.parent):s}.")
+#         shutil.copy(src, dest)
+#         if platform.system() == "Darwin":
+#             print(f"changing rpath for {str(file):s}")
+#             subprocess.check_call(cmd)
+#     except subprocess.CalledProcessError:
+#         print(" ".join(cmd) + " was not successful.")
+#     except:
+#         print(f"{file:s} was not copied.")
 
-# check rpaths for Darwin
-if platform.system() == "Darwin":
-    cmd = ["otool", "-L", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
-    try:
-        print(f"checking rpaths")
-        subprocess.check_call(cmd)
-    except subprocess.CalledProcessError:
-        print(" ".join(cmd) + " was not successful.")
+# # check rpaths for Darwin
+# if platform.system() == "Darwin":
+#     cmd = ["otool", "-L", f"./src/py{name:s}/lib{name:s}{ext[0]:s}"]
+#     try:
+#         print(f"checking rpaths")
+#         subprocess.check_call(cmd)
+#     except subprocess.CalledProcessError:
+#         print(" ".join(cmd) + " was not successful.")
 
 version = None
 with open("./VERSION", "r") as f:
     version = f.read().strip()
+
+fobj = open('./src/pycodata/version.py', "w")
+fobj.write(f"__version__ = \"{version:s}\"")
+fobj.close
 
 
 if __name__ == "__main__":
@@ -139,5 +143,6 @@ if __name__ == "__main__":
                         runtime_library_dirs=runtime_library_dirs,
                         extra_objects=extra_objects)
     setup(version=version,
-          ext_modules=[mod_constants_latest, mod_constants_2018, mod_constants_2014, mod_constants_2010, mod_version])
+         # ext_modules=[mod_constants_latest, mod_constants_2018, mod_constants_2014, mod_constants_2010, mod_version]
+         )
 
