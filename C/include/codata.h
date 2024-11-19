aa51afd5d9da0a7a56d7ff5b8418ab48d18516e2
Milan Skocic <milan.skocic@icloud.com>
2024-11-19 06:30:59 +0100

    C code in its own folder.

---
+++ C/include/codata.h
@@ -0,0 +1,13 @@
+#ifndef CODATA_H
+#define CODATA_H
+#if _MSC_VER
+#define ADD_IMPORT __declspec(dllimport)
+#else
+#define ADD_IMPORT
+#endif
+#include "codata_constants_type.h"
+#include "codata_constants_2010.h"
+#include "codata_constants_2014.h"
+#include "codata_constants_2018.h"
+#include "codata_constants_2022.h"
+#endif
