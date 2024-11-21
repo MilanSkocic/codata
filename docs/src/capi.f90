f53a1b0420d79d236d21bdece140019a5941cc61
Milan Skocic <milan.skocic@icloud.com>
2024-11-21 21:19:12 +0100

    Update online doc.

--- docs/src/capi.f90
+++
@@ -1,6 +0,0 @@
-module codata__capi
-    !! Main module for the Codata library.
-use codata__capi_version
-use codata__capi_constants
-implicit none
-end module codata__capi
--- docs/src/codata.f90
+++ docs/src/codata.f90
@@ -2,7 +2,6 @@ module codata
     !! Main module for the Codata library.
 use codata__version
 use codata__constants
-use codata__capi
 implicit none
 
 end module codata
