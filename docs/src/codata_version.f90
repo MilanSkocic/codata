644d55c2152eb8e9569227313437ee4fff424ff0
Milan Skocic <milan.skocic@icloud.com>
2024-11-21 22:24:57 +0100

    Update online documentation.

--- docs/src/codata_version.f90
+++ docs/src/codata_version.f90
@@ -1,28 +1,6 @@
 module codata__version
     !! Version
-use iso_fortran_env
 implicit none
 private
-
-character(len=*), parameter :: version = "1.2.2"
-character(len=:), allocatable, target :: version_f
-
-public :: get_version
-
-contains
-
-function get_version()result(fptr)
-    !! Get the version.
-    implicit none
-    character(len=:), pointer :: fptr 
-        !! Fortran pointer to a string indicating the version.
-
-    if(allocated(version_f))then
-        deallocate(version_f)
-    endif
-    allocate(character(len=len(version)) :: version_f)
-    version_f = version
-    fptr => version_f
-end function
-
+character(len=*), parameter :: version = "2.0.0"
 end module codata__version
