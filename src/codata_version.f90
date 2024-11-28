6c616cbb0f759da89ce21096c44078cea72bd6a7
Milan Skocic <milan.skocic@icloud.com>
2024-11-28 20:54:12 +0100

    Fix bug for version in Fortran code.

--- src/codata_version.f90
+++ src/codata_version.f90
@@ -2,5 +2,5 @@ module codata__version
     !! Version
 implicit none
 private
-character(len=*), parameter :: version = "2.0.0"
+character(len=*), parameter, public :: version = "2.0.0"
 end module codata__version
