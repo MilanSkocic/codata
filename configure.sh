092ef434fd4254bd62a6670bfd5eed6e718669d5
Milan Skocic <mskocic@Ctech-cre.local>
2025-04-09 06:23:44 +0200

    Change FFLAGS and CFLAGS.
    Change Fortran -std=2018.
    Add -fPIC for C code.

--- configure.sh
+++ configure.sh
@@ -25,8 +25,8 @@ export PY=python
 export PYGEN=python
 export BUILD_DIR="./build"
 export INCLUDE_DIR="./include"
-export FPM_FFLAGS="-std=f2008 -pedantic -Wall -Wextra"
-export FPM_CFLAGS="-std=c11 -pedantic -Wall -Wextra"
+export FPM_FFLAGS="-fPIC -std=f2018 -pedantic -Wall -Wextra"
+export FPM_CFLAGS="-fPIC -std=c11 -pedantic -Wall -Wextra"
 export FPM_LDFLAGS=""
 export DEFAULT_INSTALL_DIR="$HOME/.local"
 export PLATFORM="linux"
