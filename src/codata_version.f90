8ac6edc0ccee7b5c66f8901d383a03b4b8e0f868
Milan Skocic <milan.skocic@gmail.com>
2025-11-29 09:19:43 +0100

    Switch to dev version.

--- src/codata_version.f90
+++ src/codata_version.f90
@@ -2,5 +2,5 @@ module codata__version
     !! Version
     implicit none
     private
-    character(len=*), parameter, public :: version = "2.3.1"
+    character(len=*), parameter, public :: version = "2.3.2dev0"
 end module codata__version
