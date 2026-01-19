aae5086bd54333b5db43c9b9093e599ee6bc13f5
Milan Skocic <milan.skocic@gmail.com>
2026-01-19 20:59:57 +0100

    Add codata app to py wrapper as pycodata.

--- py/src/pycodata/__init__.py
+++ py/src/pycodata/__init__.py
@@ -6,8 +6,22 @@ Older values can be used and they feature the year as a suffix in their name.
 The latest values are available at the top level and 
 older values are available in dedicated modules.
 """
+import platform
+import subprocess
+import sys
+from pathlib import Path
+
 from .version import __version__
 from .constants_2022 import *
 from .constants_2018 import *
 from .constants_2014 import *
 from .constants_2010 import *
+
+
+def main():
+    binary = Path(__file__).parent / "codata"
+    if platform.system() == "Windows":
+        binary = binary.with_suffix(".exe")
+
+    res = subprocess.run([binary] + sys.argv[1:])
+    sys.exit(res.returncode)
