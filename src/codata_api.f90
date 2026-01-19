83962499fdb2cae7d7e899e9f65e9ccbf53c8dc0
Milan Skocic <milan.skocic@gmail.com>
2026-01-19 06:41:03 +0100

    Update sources.

--- src/codata_api.f90
+++ src/codata_api.f90
@@ -1,21 +1,19 @@
 module codata__api
     !! API.
-use codata__version, only: version
-implicit none
-private
+    use codata__version, only: version
+    implicit none
+    private
 
-character(len=:), allocatable, target, private :: version_f ! Version
+    character(len=:), allocatable, target, private :: version_f ! Version
 
-public :: get_version
+    public :: get_version
 
 contains
 
 
 function get_version()result(fptr)
     !! Get the version.
-
     character(len=:), pointer :: fptr !! Fortran pointer to a string indicating the version.
-
     if(allocated(version_f))then
         deallocate(version_f)
     endif
