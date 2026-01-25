2428f03f818f84ae2847c2ca67800f8a25d66c90
Milan Skocic <milan.skocic@gmail.com>
2026-01-25 07:54:28 +0100

    Prepare 2.4

--- src/codata_version.f90
+++ src/codata_version.f90
@@ -2,5 +2,5 @@ module codata__version
     !! Version
     implicit none
     private
-    character(len=*), parameter, public :: version = "2.4.0rc1"
+    character(len=*), parameter, public :: version = "2.4.0rc2"
 end module codata__version
