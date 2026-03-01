bdfdb0a8705720848c339a46e2740c556a3d7241
Milan Skocic <milan.skocic@gmail.com>
2026-03-01 08:30:59 +0100

    Code cleaning.
    Remove indentation at module level.

--- test/test_constants.f90
+++ test/test_constants.f90
@@ -220,26 +220,26 @@ end subroutine
 end module test_constants
 
 program tester
-    use iso_fortran_env
-    use testdrive, only : run_testsuite, new_testsuite, testsuite_type
-    use test_constants, only : collect_constants
-    implicit none
-    type(testsuite_type), allocatable :: testsuites(:)
-    character(len=*), parameter :: fmt = '("#", *(1x, a))'
-    integer :: stat, is
+use, intrinsic :: iso_fortran_env, only: error_unit
+use testdrive, only : run_testsuite, new_testsuite, testsuite_type
+use test_constants, only : collect_constants
+implicit none(type,external)
+type(testsuite_type), allocatable :: testsuites(:)
+character(len=*), parameter :: fmt = '("#", *(1x, a))'
+integer :: stat, is
 
-    stat = 0
+stat = 0
 
-    testsuites = [new_testsuite("constants", collect_constants)]
+testsuites = [new_testsuite("constants", collect_constants)]
 
-    do is = 1, size(testsuites)
-        write(error_unit, fmt) "Testing:", testsuites(is)%name
-        call run_testsuite(testsuites(is)%collect, error_unit, stat)
-    end do
+do is = 1, size(testsuites)
+    write(error_unit, fmt) "Testing:", testsuites(is)%name
+    call run_testsuite(testsuites(is)%collect, error_unit, stat)
+end do
 
-    if (stat > 0) then
-        write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
-        error stop
-    end if
+if (stat > 0) then
+    write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+    error stop
+end if
 
 end program
