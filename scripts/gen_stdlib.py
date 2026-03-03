6bf303d0c92c55f1074830a6799b443b6347c8b3
Milan Skocic <milan.skocic@gmail.com>
2026-03-03 15:01:27 +0100

    Update the generation of stdlib module for removing C API and array.

--- scripts/gen_stdlib.py
+++ scripts/gen_stdlib.py
@@ -2,18 +2,23 @@
 r"""Generate sources for stdlib."""
 import argparse
 
+newline = "\n"
 
 def run(fpath_ast: str, fpath_code: str)->None:
     fast = open(fpath_ast, "r")
     fcode = open(fpath_code, "w")
+    copy = True
 
-    for line in fast:
-        new_line = line.replace("codata__constants_type", "stdlib_codata_type")
-        new_line = new_line.replace("codata__constants_2022", "stdlib_codata")
-        if "!capi" in new_line:
-            new_line=""
-        fcode.write(new_line)
-
+    for i, line in enumerate(fast):
+        if line.startswith("!=====") and i > 100:
+            copy = False
+        if copy:
+            new_line = line.replace("codata__constants_type", "stdlib_codata_type")
+            new_line = new_line.replace("codata__constants_2022", "stdlib_codata")
+            if "!capi" in new_line:
+                new_line=""
+            fcode.write(new_line)
+    fcode.write("!======================================================================="+newline)
     fast.close()
     fcode.close()
 
