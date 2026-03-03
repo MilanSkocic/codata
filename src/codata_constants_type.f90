4c32dcd0fa289d81b1cc8bbd09f261a2cb6b85b1
Milan Skocic <milan.skocic@gmail.com>
2026-03-03 05:31:57 +0100

    Refactoring.
    Add comment styling for separating functions and headers.

--- src/codata_constants_type.f90
+++ src/codata_constants_type.f90
@@ -11,32 +11,38 @@ private
 ! CODATA_CONSTANT_TYPE
 !=======================================================================
 type :: codata_constant_type
-    !! Derived type for representing a Codata constant.
-    character(len=64) :: name ! Name of the constant
-    real(dp) :: value         ! Value of the constant
-    real(dp) :: uncertainty   ! Uncertainty of the constant
-    character(len=32) :: unit ! Unit of the constant
+!! Derived type for representing a Codata constant.
+character(len=64) :: name ! Name of the constant
+real(dp) :: value         ! Value of the constant
+real(dp) :: uncertainty   ! Uncertainty of the constant
+character(len=32) :: unit ! Unit of the constant
 contains 
-    procedure :: print
-    procedure :: to_real_sp
-    procedure :: to_real_dp
-    generic :: to_real => to_real_sp, to_real_dp
+procedure :: print
+procedure :: to_real_sp
+procedure :: to_real_dp
+generic :: to_real => to_real_sp, to_real_dp
 end type codata_constant_type
 !-----------------------------------------------------------------------
 type, bind(C) :: capi_constant_type
-    !! Derived type for representing a Codata constant in C.
-    character(kind=c_char) :: name(65)
-    real(c_double) :: value
-    real(c_double) :: uncertainty
-    character(kind=c_char) :: unit(33)
+!! Derived type for representing a Codata constant in C.
+character(kind=c_char) :: name(65)
+real(c_double) :: value
+real(c_double) :: uncertainty
+character(kind=c_char) :: unit(33)
 end type capi_constant_type
 !=======================================================================
 
+
+!=======================================================================
+! INTERFACES
+!=======================================================================
 interface to_real
-    !! Get the constant value or uncertainty.
-    module procedure to_real_sp
-    module procedure to_real_dp
+!! Get the constant value or uncertainty.
+module procedure to_real_sp
+module procedure to_real_dp
 end interface
+!=======================================================================
+
 
 !=======================================================================
 ! PUBLIC
@@ -48,45 +54,56 @@ public :: capi_constant_type
 
 contains
 
+!=======================================================================
+! PRINT
+!=======================================================================
 subroutine print(self)
-    !! Print out the constant's name, value, uncertainty and unit.
-    class(codata_constant_type), intent(in) :: self
-    print "(A64, SP, "//FMT_REAL_DP//", A5, "//FMT_REAL_DP//", 1X, A32)", self%name, self%value, "+/-", self%uncertainty, self%unit 
+!! Print out the constant's name, value, uncertainty and unit.
+class(codata_constant_type), intent(in) :: self
+print "(A64, SP, "//FMT_REAL_DP//", A5, "//FMT_REAL_DP//", 1X, A32)", &
+        self%name, self%value, "+/-", self%uncertainty, self%unit 
 end subroutine print
+!=======================================================================
 
+
+!=======================================================================
+! TO_REAL
+!=======================================================================
 elemental pure real(sp) function to_real_sp(self, mold, uncertainty) result(r)
-    !! Get the constant value or uncertainty for the kind sp
-    
-    class(codata_constant_type), intent(in) :: self !! Codata constant
-    real(sp), intent(in) :: mold !! dummy argument to disambiguate at compile time the generic interface
-    logical, intent(in), optional :: uncertainty !! Set to true if the uncertainty is required. Default to .false..
-        !! 
-    logical :: u
-    
-    u = optval(uncertainty, .false.)
+!! Get the constant value or uncertainty for the kind sp
 
-    if(u .eqv. .false.)then
-        r = real(self%value, kind(mold))
-    else
-        r = real(self%uncertainty, kind(mold))
-    end if
+class(codata_constant_type), intent(in) :: self !! Codata constant
+real(sp), intent(in) :: mold                    !! dummy argument to disambiguate at compile time the generic interface
+logical, intent(in), optional :: uncertainty    !! Set to true if the uncertainty is required. Default to .false..
+    !! 
+logical :: u
+
+u = optval(uncertainty, .false.)
+
+if(u .eqv. .false.)then
+    r = real(self%value, kind(mold))
+else
+    r = real(self%uncertainty, kind(mold))
+end if
 end function to_real_sp
+!-----------------------------------------------------------------------
 elemental pure real(dp) function to_real_dp(self, mold, uncertainty) result(r)
-    !! Get the constant value or uncertainty for the kind dp
-    
-    class(codata_constant_type), intent(in) :: self !! Codata constant
-    real(dp), intent(in) :: mold !! dummy argument to disambiguate at compile time the generic interface
-    logical, intent(in), optional :: uncertainty !! Set to true if the uncertainty is required. Default to .false..
-        !! 
-    logical :: u
-    
-    u = optval(uncertainty, .false.)
+!! Get the constant value or uncertainty for the kind dp
 
-    if(u .eqv. .false.)then
-        r = real(self%value, kind(mold))
-    else
-        r = real(self%uncertainty, kind(mold))
-    end if
+class(codata_constant_type), intent(in) :: self !! Codata constant
+real(dp), intent(in) :: mold                    !! dummy argument to disambiguate at compile time the generic interface
+logical, intent(in), optional :: uncertainty    !! Set to true if the uncertainty is required. Default to .false..
+    !! 
+logical :: u
+
+u = optval(uncertainty, .false.)
+
+if(u .eqv. .false.)then
+    r = real(self%value, kind(mold))
+else
+    r = real(self%uncertainty, kind(mold))
+end if
 end function to_real_dp
+!-----------------------------------------------------------------------
 
 end module codata__constants_type
