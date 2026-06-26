b6569f79f24710838c0f8e6adff1e6046c6aeba2
Milan Skocic <milan.skocic@gmail.com>
2026-06-26 10:51:45 +0200

    Start creating app struct for C version of the CLI.

--- app/main.c
+++ app/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "codata.h"
 
+#define VERSION_SIZE 256
+
+static void version_text(){
+    char v[VERSION_SIZE];
+    strcpy(v, "codata ");
+    strcat(v, codata_version());
+    strcat(v, "\n\n");
+    strcat(v, "Copyright (c) 2022 Milan Skocic\n");
+    strcat(v, "License MIT\n");
+    strcat(v, "\n");
+    strcat(v, "Wrtten by Milan Skocic\n");
+    printf("%s\n", v);
+}
 
 
-int main(){
-    
+
+int main(int argc, char **argv){
     struct codata_constant_type *ccp = cc_2010; 
-
+    
+    version_text();
     printf("%s\n", cc[1].name);
 
     return EXIT_SUCCESS;
